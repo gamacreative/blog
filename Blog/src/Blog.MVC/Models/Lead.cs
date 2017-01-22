9a1fccddef2462ad7b709b00addbafd850a04972
fabioniglio <fabio_niglio@hotmail.com>
2017-01-22 19:29:05 -0300

    Validação leads
    O cadastro dos leads possuem campos obrigatórios.

--- Blog/src/Blog.MVC/Models/Lead.cs
+++ Blog/src/Blog.MVC/Models/Lead.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,19 @@ namespace Blog.MVC.Models
     public class Lead
     {
         public int ID { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(150, MinimumLength = 3)]
+        [Required(ErrorMessage = "Nome obrigatório")]
+        [Display(Name = "Nome*")]
         public string Name { get; set; }
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Email inválido")]
+        [Required(ErrorMessage = "Email obrigatório")]
         public string Email { get; set; }
+        [Display(Name = "Estado")]
+        [Required(ErrorMessage = "Estado obrigatório")]
         public string State { get; set; }
+        [Display(Name = "Cidade")]
+        [Required(ErrorMessage = "Cidade obrigatória")]
         public string City { get; set; }
         public string IP { get; set; }
         public int DownloadCount { get; set; }
--- Blog/src/Blog.MVC/wwwroot/_references.js
+++ Blog/src/Blog.MVC/wwwroot/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="js/jquery.validate.js" />
+/// <reference path="js/jquery-3.1.1.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
