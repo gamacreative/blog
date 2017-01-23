b7655bc3d9943be7e345191bfaa5e64cc9b06017
guilhermebaldini@hotmail.com <VOC6w6r7u>
2017-01-22 22:16:33 -0200

    eBook correction

--- Blog/src/Blog.MVC/Controllers/eBookController.cs
+++ Blog/src/Blog.MVC/Controllers/eBookController.cs
@@ -27,12 +27,19 @@ namespace Blog.MVC.Controllers
         [HttpPost]
         public IActionResult Create(Lead model)
         {
-            SendEmail(model.Email, "eBook", "Teste eBook");
-            //TODO: validações
-            model.IP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
-            _context.Leads.Add(model);
-            _context.SaveChanges();
+            try
+            {
+                //SendEmail(model.Email, "eBook", "Teste eBook");
+                //TODO: validações
+                model.IP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+                _context.Leads.Add(model);
+                _context.SaveChanges();
+            }
+            catch (System.Exception)
+            {
 
+                throw;
+            }
             return View(model);
         }
 
